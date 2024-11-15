49f729993786342f24156779f748a43751c21a19
arshad012 <forarshad99@gmail.com>
2024-11-15 17:04:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,21 +10,24 @@ tr.append(td);
 output.append(tr);
 
 const p1 = new Promise((resolve) => {
+	let delay = 2000;
 	setTimeout(() => {
-		resolve('First Promise');
-	}, 1000);
+		resolve(['First Promise', delay]);
+	}, delay);
 })
 
 const p2 = new Promise((resolve) => {
+	let delay = 1000;
 	setTimeout(() => {
-		resolve('Second Promise');
-	}, 2000);
+		resolve(['Second Promise', delay]);
+	}, delay);
 })
 
 const p3 = new Promise((resolve) => {
+	let delay = 3000;
 	setTimeout(() => {
-		resolve('Third Promise');
-	}, 3000);
+		resolve(['Third Promise', delay]);
+	}, delay);
 })
 
 Promise.all([p1, p2, p3])
@@ -34,16 +37,17 @@ Promise.all([p1, p2, p3])
 })
 
 
-
 function modifyTable(data) {
+	// console.log(data);
 	output.innerHTML = null;
 
 	data.forEach((el, index) => {
+		// console.log(el);
 		const tr = document.createElement('tr');
 		const td1 = document.createElement('td');
-		td1.innerText = el;
+		td1.innerText = el[0];
 		const td2 = document.createElement('td');
-		td2.innerText = index+1;
+		td2.innerText = el[1];
 
 		tr.append(td1, td2);
 		output.append(tr);
